af61e251309b2b2e2c4b5805afebd51ae6358849
xuki <xumingjing@corp-ci.com>
2019-07-14 10:45:25 +0800

    添加公众号代码

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -9,6 +9,7 @@
 namespace App\Http\Controllers;
 
 
+use EasyWeChatComposer\EasyWeChat;
 use Illuminate\Support\Facades\Log;
 
 class WeChatController extends Controller
@@ -21,6 +22,7 @@ class WeChatController extends Controller
     public function serve()
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
 
         $app = app('wechat.official_account');
         $app->server->push(function($message){
