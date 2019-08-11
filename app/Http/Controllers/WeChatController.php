8c64ec9e2dbe8c5ce3d398fa246707b4ec45eb04
xuki <xuki.xmj@foxmail.com>
2019-08-11 08:30:59 +0800

    最后一次提交

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -32,27 +32,43 @@ class WeChatController extends Controller
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
-        $this->createMenu();
-
+        //$this->createMenu();
+        
         $this->app->server->push(function ($message) {
-            Log::info($message);
-            return 'hello';
-            Log::info('message is '.$message);
-            $userService = $this->app->user;
-            $user = $userService->get($message->FromUserName);
-            switch ($message->MsgType) {
+                Log::info($message);
+            //Log::info('message is '.$message);
+            //$userService = $this->app->user;
+            //$user = $userService->get($message->FromUserName);
+            switch ($message['MsgType']) {
                 case 'event':
                     switch ($message->Event) {
                         case 'subscribe':
-                            $this->app->staff->message(new Text(
-                                $user."您好，终于找到我们啦～河马赴美为你准备了各种攻略，回复下列数字查看详细内容^ ^\n1、了解河马赴美（回复1）\n2、赴美流程（回复2）\n3、套餐详情（回复3）\n4、月子中心各房型介绍（回复4）\n5、美国签证流程（回复5）\n6、如何选择医院（回复6）\n7、如何选择医生（回复7）\n8、赴美生子百人真实diy经验分享：传送门（回复8）\n9、赴美生子如何领取生育津贴（回复9）\n10、赴美生子孕妈收藏的尔湾儿科诊所收费信息 ＋ 疫苗品牌（回复10）\n11、尔湾幼儿园入园指南（回复11）\n12、美国美食推荐（回复12）\n13、美国景点推荐（回复13）"
-                            ))->to($message->FromUserName)->send();
+                            return '欢迎订阅';
                             break;
                         case 'unsubscribe':
                             break;
                     }
                     break;
                 case 'text':
+                    $result_name=$message['FromUserName'].'_result.txt';
+                    $result_file=resource_path($result_name);
+                    if(file_exists($result_file)){
+                        $content=file_get_contents($result_file);
+                        unlink($result_file);
+                        return $content;
+                    }
+                    return '您可以尝试发送一张纯文字的图，可能会出现“该公众号提供的服务出现故障，请稍后再试”。Be easy。您可以等10秒之后随便发文字即可以获得图片中的文字，Good luck to you!(受限于订阅号的功能，都没动力搞下去了~)';
+                    break;
+                case 'image':
+                    $image_name=$message['FromUserName'];
+                    $content=file_get_contents($message['PicUrl']);
+                    $myfile = fopen(resource_path($image_name), "w") or die("Unable to open file!");
+                    fwrite($myfile,$content);
+                    fclose($myfile);
+                    $shell=sprintf('tesseract %s %s -l chi_sim+eng','/var/www/wechat/resources/'.$image_name,'/var/www/wechat/resources/'.$image_name.'_result');
+                    system($shell);
+                    return file_get_contents('/var/www/wechat/resources/result');
+                    //return '10秒之后随便发一个文字即可以获得图片中的文字';
                     break;
             }
         });
@@ -139,7 +155,7 @@ class WeChatController extends Controller
                 ],
             ],
         ];
-        $menu->add($buttons);
+        $menu->create($buttons);
     }
 
 
@@ -150,6 +166,6 @@ class WeChatController extends Controller
     public function menu_destroy()
     {
         $menu = $this->app->menu;
-        $menu->destroy();
+        $menu->delete();
     }
 }
